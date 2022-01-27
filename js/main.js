c2060d9051cdd4039749c91ff80e4a3da4744294
phidabezt <quangphi1231@gmail.com>
2022-01-27 11:11:12 +0700

    fix side bar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* TRUNCATE TITLE */
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     truncateCardTitle();
 });
 
 function truncateCardTitle() {
     var cardList = document.getElementsByClassName("card-title");
     console.log(cardList);
-    for(var i = 0; i < cardList.length; i++) {
+    for (var i = 0; i < cardList.length; i++) {
         var trunc_text = cardList[i].innerHTML;
         var new_text = truncateString(trunc_text, 30);
         cardList[i].innerHTML = new_text;
@@ -16,26 +16,48 @@ function truncateCardTitle() {
 function truncateString(str, max_length) {
     if (str.length > max_length) {
         return str.slice(0, max_length) + "...";
-    } else 
+    } else
         return str;
 }
 
 /* SIDEBAR MINI */
+function outsideClick(event, notelem)	{
+    notelem = $(notelem); // jquerize (optional)
+    // check outside click for multiple elements
+    var clickedOut = true, i, len = notelem.length;
+    for (i = 0;i < len;i++)  {
+        if (event.target == notelem[i] || notelem[i].contains(event.target)) {
+            clickedOut = false;
+        }
+    }
+    return clickedOut;
+}
+
 var toggleBtn = document.querySelector(".sidebarMini__button");
 var sidebarMini = document.querySelector(".sidebarMini");
 var switchBtn = document.querySelector("#checkbox");
 
-toggleBtn.addEventListener("click", function(){
+window.addEventListener('click', function(e) {
+    if (outsideClick(e, sidebarMini)) {
+        sidebarMini.classList.remove('is-opened');
+    }
+ });
+
+toggleBtn.addEventListener("click", function () {
     sidebarMini.classList.toggle('is-opened');
 });
 
-switchBtn.addEventListener("click", function(){
+switchBtn.addEventListener("click", function () {
     document.querySelector("body").classList.toggle('darkMode');
+
 });
+
+
+
 
 /* RESPONSIVE NAVBAR */
 var switchNavBar = document.querySelector("#checkboxNavBar");
 
-switchNavBar.addEventListener("click", function(){
+switchNavBar.addEventListener("click", function () {
     document.querySelector(".navbar-collapse").classList.remove("show");
 });
